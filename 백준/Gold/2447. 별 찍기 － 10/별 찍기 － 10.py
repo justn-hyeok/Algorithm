9e572add44d3b2c5ba6e72f13df32f75fea7ad4f
황준혁 <justn.hyeok@gmail.com>
2024-09-18 22:59:20 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 40556 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,18 @@
+def draw_stars(n):
+    if n == 1:
+        return ['*']
+
+    stars = draw_stars(n // 3)
+    result = []
+
+    for s in stars:
+        result.append(s * 3)
+    for s in stars:
+        result.append(s + ' ' * (n // 3) + s)
+    for s in stars:
+        result.append(s * 3)
+
+    return result
+
+n = int(input())
+print('\n'.join(draw_stars(n)))
